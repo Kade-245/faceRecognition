effce17932604b811201b8017c5485c804d4e5fa
Bargav Krishna <bargavkrishna26@gmail.com>
2022-03-02 23:00:04 +0530

    commit 1

--- faceR.py
+++ faceR.py
@@ -1,22 +1,43 @@
-import numpy as np
-import face_recognition as fr
+from tkinter import *
+import numpy as np 
+import face_recognition as fr 
+from PIL import Image, ImageTk
 import cv2
 
-video_capture = cv2.VideoCapture(0)
+# Create an instance of TKinter Window or frame
+win = Tk()
 
-bruno_image = fr.load_image_file("myself.jpg")
+# Set the size of the window
+win.geometry("700x700")
+
+# Create a Label to capture the Video frames
+label =Label(win)
+cam= cv2.VideoCapture(0)
+
+#training stuff
+my_image = fr.load_image_file("myself.jpg")
 mam_image = fr.load_image_file("madam1.jpg")
 
-bruno_face_encoding = fr.face_encodings(bruno_image)[0]
+my_face_encoding = fr.face_encodings(my_image)[0]
 mam_face_encoding = fr.face_encodings(mam_image)[0]
 
 
-known_face_encondings = [bruno_face_encoding,mam_face_encoding]
+known_face_encondings = [my_face_encoding,mam_face_encoding]
 known_face_names = ["Bargav","Correspondent mam"]
 
-while True: 
-    ret, frame = video_capture.read()
+# Define function to show frame
+def show_frames():
+   # Get the latest frame and convert into Image
+   cv2image= cv2.cvtColor(cam.read()[1],cv2.COLOR_BGR2RGB)
+   img = Image.fromarray(cv2image)
+   # Convert image to PhotoImage
+   imgtk = ImageTk.PhotoImage(image = img)
+   label.imgtk = imgtk
+   label.configure(image=imgtk)
+   label.after(20, show_frames)
 
+def capture():
+    frame = cam.read()
     rgb_frame = frame[:, :, ::-1]
 
     face_locations = fr.face_locations(rgb_frame)
@@ -41,10 +62,8 @@ while True:
         cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
 
-    cv2.imshow('Webcam_facerecognition', frame)
+cpt = Button(win , text="Capture" , command=capture)
+cpt.pack()
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-video_capture.release()
-cv2.destroyAllWindows()
+show_frames()
+win.mainloop()
